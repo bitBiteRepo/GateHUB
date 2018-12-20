6418e1b6773afb26fd2975e6c93f5458720067ae
amitsherman <amit.sherman@gmail.com>
2018-12-20 10:14:01 +0200

    Initial commit

---
+++ GateHUB/GateHUB.h
@@ -0,0 +1,19 @@
+//
+//  GateHUB.h
+//  GateHUB
+//
+//  Created by Amit Sherman on 12/20/18.
+//  Copyright © 2018 gatewise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GateHUB.
+FOUNDATION_EXPORT double GateHUBVersionNumber;
+
+//! Project version string for GateHUB.
+FOUNDATION_EXPORT const unsigned char GateHUBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GateHUB/PublicHeader.h>
+
+
---
+++ GateHUB/MyClass.swift
@@ -0,0 +1,16 @@
+//
+//  MyClass.swift
+//  GateHUB
+//
+//  Created by Amit Sherman on 12/20/18.
+//  Copyright © 2018 gatewise. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+class MyClass{
+    var x : Int
+    init() {
+        x=1
+    }
+}
